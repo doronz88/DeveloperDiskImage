265d264f72d40512728a4ad20ae84525724d3199
rob-X1 <rob-X1@users.noreply.github.com>
2025-07-21 10:42:47 +0200

    Allow to specify a GitHub access token to avoid GithubRateLimitExceededError

--- developer_disk_image/repo.py
+++ developer_disk_image/repo.py
@@ -29,10 +29,11 @@ class DeveloperDiskImageRepository:
     def create(cls) -> 'DeveloperDiskImageRepository':
         return cls(cls._query(DEVELOPER_DISK_IMAGE_REPO_TREE_URL)['tree'])
 
-    def __init__(self, tree: Mapping):
+    def __init__(self, tree: Mapping, github_token: str=None):
         self._path_urls = {}
         for node in tree:
             self._path_urls[node['path']] = node
+        self.github_token = github_token
 
     def get_developer_disk_image(self, version: str) -> Optional[DeveloperDiskImage]:
         image = self._get_blob(f'DeveloperDiskImages/{version}/DeveloperDiskImage.dmg')
@@ -54,11 +55,19 @@ class DeveloperDiskImageRepository:
         url = self._path_urls.get(path, {}).get('url')
         if url is None:
             return None
-        return base64.b64decode(self._query(url)['content'])
+        return base64.b64decode(self._query(url, github_token=self.github_token)['content'])
 
     @staticmethod
-    def _query(url: str) -> Mapping:
-        response = json.loads(requests.get(url).text)
+    def _query(url: str, github_token: str = None) -> Mapping:
+        headers = {}
+        if github_token is not None:
+            headers = {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': 'Bearer ' + github_token,
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        response = json.loads(requests.get(url, headers=headers).text)
         if response.get('message', '').startswith('API rate limit exceeded'):
-            raise GithubRateLimitExceededError()
+            raise GithubRateLimitExceededError(
+                "GitHub API rate limit exceeded. Wait some minutes or use a custom GitHub access token")
         return response
