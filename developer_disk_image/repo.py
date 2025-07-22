e9e095ea6200f128f078b5f493146a830dd96c96
rob-X1 <rob-X1@users.noreply.github.com>
2025-07-22 23:05:24 +0200

    repo: Allow to specify a GitHub access token
    * Allow to specify a GitHub access token to avoid `GithubRateLimitExceededError`
    
    * `GithubRateLimitExceededError`: include date/time when rate limit will be reset

--- developer_disk_image/repo.py
+++ developer_disk_image/repo.py
@@ -1,6 +1,7 @@
 import base64
 import dataclasses
 import json
+from datetime import datetime, timezone
 from typing import Mapping, Optional
 
 import requests
@@ -29,10 +30,11 @@ class DeveloperDiskImageRepository:
     def create(cls) -> 'DeveloperDiskImageRepository':
         return cls(cls._query(DEVELOPER_DISK_IMAGE_REPO_TREE_URL)['tree'])
 
-    def __init__(self, tree: Mapping):
+    def __init__(self, tree: Mapping, github_token: Optional[str] = None):
         self._path_urls = {}
         for node in tree:
             self._path_urls[node['path']] = node
+        self.github_token = github_token
 
     def get_developer_disk_image(self, version: str) -> Optional[DeveloperDiskImage]:
         image = self._get_blob(f'DeveloperDiskImages/{version}/DeveloperDiskImage.dmg')
@@ -54,11 +56,23 @@ class DeveloperDiskImageRepository:
         url = self._path_urls.get(path, {}).get('url')
         if url is None:
             return None
-        return base64.b64decode(self._query(url)['content'])
+        return base64.b64decode(self._query(url, github_token=self.github_token)['content'])
 
     @staticmethod
-    def _query(url: str) -> Mapping:
-        response = json.loads(requests.get(url).text)
-        if response.get('message', '').startswith('API rate limit exceeded'):
-            raise GithubRateLimitExceededError()
-        return response
+    def _query(url: str, github_token: Optional[str] = None) -> Mapping:
+        headers = {}
+        if github_token is not None:
+            headers = {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': 'Bearer ' + github_token,
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        response = requests.get(url, headers=headers)
+        content = json.loads(response.text)
+        if content.get('message', '').startswith('API rate limit exceeded'):
+            reset_time = int(response.headers['X-RateLimit-Reset'])
+            reset_utc = datetime.fromtimestamp(reset_time, timezone.utc)
+            reset_local = reset_utc.astimezone()
+            raise GithubRateLimitExceededError(
+                f'GitHub API rate limit exceeded. Wait until {reset_local} or use a custom GitHub access token')
+        return content
